37e4fa0814cce211a89f22e058de87bc42bef77b
ageron joachim <ageron.joachim@gmail.com>
2025-07-22 16:58:25 +0200

    ✨ feat: implement Matomo tracking using Script component
    - Refactor Matomo component to use next/script for loading
    - Simplify tracking setup and improve performance

--- app/ui/components/Matomo.tsx
+++ app/ui/components/Matomo.tsx
@@ -1,35 +1,26 @@
-"use client";
+import Script from "next/script";
 
-import { useEffect } from "react";
 
 export default function Matomo() {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      window._paq = window._paq || [];
-      window._paq.push(["trackPageView"]);
-      window._paq.push(["enableLinkTracking"]);
+  
 
-      (function () {
-        const u = "//matomo.ageronjoachim.com/";
-        window._paq.push(["setTrackerUrl", u + "matomo.php"]);
-        window._paq.push(["setSiteId", "1"]);
-        const d = document;
-        const g = d.createElement("script");
-        const s = d.getElementsByTagName("script")[0];
-        g.async = true;
-        g.src = u + "matomo.js";
-        if (s.parentNode) {
-          s.parentNode.insertBefore(g, s);
-        }
-      })();
-    }
-  }, []);
-
-  return null;
-}
-
-declare global {
-  interface Window {
-    _paq: any[];
-  }
+  return (
+    <>
+	<Script strategy="afterInteractive" id="matomoAnalytics">
+		{`
+		var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//matomo.ageronjoachim.com/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+		`}
+	</Script>
+</>
+  );
 }
